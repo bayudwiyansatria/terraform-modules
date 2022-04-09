8eafba9138d9edc8b8f4b0cc1e1ce826af113f41
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2022-04-10 02:48:16 +0800

    Add Keycloak helm

---
+++ terraform-helm-keycloak/main.tf
@@ -0,0 +1,27 @@
+locals {
+  keycloak_values_default_values = [
+    file("${path.module}/files/default.yml"),
+    file("${path.module}/files/system.yml"),
+    file("${path.module}/files/docker.yml"),
+    file("${path.module}/files/network.yml"),
+    file("${path.module}/files/ingress.yml"),
+    file("${path.module}/files/resource.yml"),
+  ]
+
+  keycloak_values_values = [for i in var.keycloak_values : i]
+
+  keycloak_values = concat(local.keycloak_values_default_values, local.keycloak_values_values)
+
+}
+
+resource "helm_release" "keycloak" {
+  name       = "keycloak"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "keycloak"
+  version    = "7.1.12"
+
+  create_namespace = true
+  namespace        = "keycloak"
+
+  values = local.keycloak_values
+}
