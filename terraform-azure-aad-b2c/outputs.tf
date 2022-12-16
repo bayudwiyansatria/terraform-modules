c751e6a14462179cc0bb7a26d3a86094f9c2814b
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2022-12-16 14:17:59 +0800

    Rename Azure AD B2C

--- terraform-azure-aad-b2c/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "id" {
-  value = azurerm_aadb2c_directory.aad.id
-}
-
-output "tenant_id" {
-  value = azurerm_aadb2c_directory.aad.tenant_id
-}
-
---
+++ terraform-azure-active-directory-b2c/outputs.tf
@@ -0,0 +1,8 @@
+output "id" {
+  value = azurerm_aadb2c_directory.tenant.id
+}
+
+output "tenant_id" {
+  value = azurerm_aadb2c_directory.tenant.tenant_id
+}
+
