4243c9054c0e10dfd6c112ba5b920aed969f6eba
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2022-12-18 14:56:41 +0800

    Init Google Cloud SQL

---
+++ terraform-google-sql-instance/main.tf
@@ -0,0 +1,139 @@
+resource "google_sql_database_instance" "main" {
+  region               = var.region
+  database_version     = var.database_version
+  name                 = var.name
+  master_instance_name = var.master_instance_name
+  project              = var.project
+  root_password        = var.root_password
+  deletion_protection  = var.deletion_protection
+
+  dynamic "settings" {
+    for_each = var.settings
+    content {
+      tier                  = settings.value.tier
+      activation_policy     = settings.value.activation_policy
+      availability_type     = settings.value.availability_type
+      collation             = settings.value.collation
+      disk_autoresize       = settings.value.disk_autoresize
+      disk_autoresize_limit = settings.value.disk_autoresize_limit
+      disk_type             = settings.value.disk_type
+      disk_size             = settings.value.disk_size
+      pricing_plan          = settings.value.pricing_plan
+      user_labels           = settings.value.user_labels
+
+      dynamic "database_flags" {
+        for_each = settings.value.database_flags
+        content {
+          name  = database_flags.value.name
+          value = database_flags.value.value
+        }
+      }
+
+      dynamic "active_directory_config" {
+        for_each = settings.value.active_directory_config
+        content {
+          domain = active_directory_config.value.domain
+        }
+      }
+
+      dynamic "backup_configuration" {
+        for_each = settings.value.backup_configuration
+        content {
+          binary_log_enabled             = backup_configuration.value.binary_log_enabled
+          enabled                        = backup_configuration.value.enabled
+          start_time                     = backup_configuration.value.start_time
+          transaction_log_retention_days = backup_configuration.value.transaction_log_retention_days
+
+          dynamic "backup_retention_settings" {
+            for_each = backup_configuration.value.backup_retention_settings
+            content {
+              retained_backups = backup_retention_settings.value.retained_backups
+              retention_unit   = backup_retention_settings.value.retention_unit
+            }
+          }
+        }
+      }
+
+      dynamic "ip_configuration" {
+        for_each = settings.value.ip_configuration
+        content {
+          ipv4_enabled       = ip_configuration.value.ipv4_enabled
+          private_network    = ip_configuration.value.private_network
+          require_ssl        = ip_configuration.value.ipv4_enabled
+          allocated_ip_range = ip_configuration.value.allocated_ip_range
+
+          dynamic "authorized_networks" {
+            for_each = ip_configuration.value.authorized_networks
+            content {
+              name            = authorized_networks.value.name
+              value           = authorized_networks.value.value
+              expiration_time = authorized_networks.value.expiration_time
+            }
+          }
+        }
+      }
+
+      dynamic "location_preference" {
+        for_each = settings.value.location_preference
+        content {
+          follow_gae_application = location_preference.value.follow_gae_application
+          zone                   = location_preference.value.zone
+        }
+      }
+
+      dynamic "maintenance_window" {
+        for_each = settings.value.maintenance_window
+        content {
+          day          = maintenance_window.value.day
+          hour         = maintenance_window.value.hour
+          update_track = maintenance_window.value.update_track
+        }
+      }
+
+      dynamic "insights_config" {
+        for_each = settings.value.insights_config
+        content {
+          query_insights_enabled  = insights_config.value.query_insights_enabled
+          query_string_length     = insights_config.value.query_string_length
+          record_application_tags = insights_config.value.record_application_tags
+          record_client_address   = insights_config.value.record_client_address
+        }
+      }
+    }
+  }
+
+  dynamic "replica_configuration" {
+    for_each = var.replica_configuration
+    content {
+      ca_certificate            = replica_configuration.value.ca_certificate
+      client_certificate        = replica_configuration.value.client_certificate
+      client_key                = replica_configuration.value.client_key
+      connect_retry_interval    = replica_configuration.value.connect_retry_interval
+      dump_file_path            = replica_configuration.value.dump_file_path
+      failover_target           = replica_configuration.value.failover_target
+      master_heartbeat_period   = replica_configuration.value.master_heartbeat_period
+      password                  = replica_configuration.value.password
+      ssl_cipher                = replica_configuration.value.ssl_cipher
+      username                  = replica_configuration.value.username
+      verify_server_certificate = replica_configuration.value.verify_server_certificate
+    }
+  }
+
+  dynamic "restore_backup_context" {
+    for_each = var.restore_backup_context
+    content {
+      backup_run_id = restore_backup_context.value.backup_run_id
+      instance_id   = restore_backup_context.value.instance_id
+      project       = restore_backup_context.value.project
+    }
+  }
+
+  dynamic "clone" {
+    for_each = var.clone
+    content {
+      source_instance_name = clone.value.source_instance_name
+      point_in_time        = clone.value.point_in_time
+      allocated_ip_range   = clone.value.allocated_ip_range
+    }
+  }
+}
---
+++ terraform-google-sql-instance/variables.tf
@@ -0,0 +1,195 @@
+variable "region" {
+  type        = string
+  description = "The region the instance will sit in. If a region is not provided in the resource definition, the provider region will be used instead."
+}
+
+variable "settings" {
+  type = set(object({
+    tier                  = string
+    activation_policy     = string
+    availability_type     = string
+    collation             = string
+    #    connector_enforcement = bool
+    disk_autoresize       = bool
+    disk_autoresize_limit = number
+    disk_size             = number
+    disk_type             = string
+    pricing_plan          = string
+    user_labels           = map(any)
+    database_flags        = set(object({
+      name  = string
+      value = string
+    }))
+    active_directory_config = set(object({
+      domain = string
+    }))
+    #    deny_maintenance_period = set(object({
+    #      # If the year of the date is empty, the year of the date also must be empty. In this case, it means the no maintenance interval recurs every year. The date is in format yyyy-mm-dd i.e., 2020-11-01, or mm-dd, i.e., 11-01
+    #      end_date   = string
+    #      start_date = string
+    #      time       = string
+    #    }))
+    #    sql_server_audit_config = set(object({
+    #      bucket             = string
+    #      upload_interval    = string
+    #      retention_interval = string
+    #      time_zone          = string
+    #    }))
+    backup_configuration = set(object({
+      binary_log_enabled             = bool
+      enabled                        = bool
+      start_time                     = string
+      #      point_in_time_recovery_enabled = string
+      location                       = string
+      transaction_log_retention_days = number
+      backup_retention_settings      = set(object({
+        retained_backups = any
+        retention_unit   = string
+      }))
+    }))
+    ip_configuration = set(object({
+      ipv4_enabled        = bool
+      private_network     = string
+      require_ssl         = bool
+      allocated_ip_range  = string
+      authorized_networks = set(object({
+        expiration_time = string
+        name            = string
+        value           = string
+      }))
+    }))
+    location_preference = set(object({
+      follow_gae_application = string
+      zone                   = string
+      #      secondary_zone         = string
+    }))
+    maintenance_window = set(object({
+      day          = number
+      hour         = number
+      #  Receive updates earlier (canary) or later (stable)
+      update_track = string
+    }))
+    insights_config = set(object({
+      query_insights_enabled  = bool
+      query_string_length     = number
+      record_application_tags = bool
+      record_client_address   = bool
+      #      query_plans_per_minute  = number
+    }))
+    #    password_validation_policy = set(object({
+    #      min_length                  = bool
+    #      complexity                  = bool
+    #      reuse_interval              = number
+    #      disallow_username_substring = bool
+    #      password_change_interval    = number
+    #      enable_password_policy      = bool
+    #    }))
+
+  }))
+  description = "The settings to use for the database. The configuration is detailed below"
+  default     = [
+    {
+      tier                    = "db-f1-micro"
+      activation_policy       = "ALWAYS"
+      availability_type       = "ZONAL"
+      collation               = null
+      connector_enforcement   = false
+      disk_autoresize         = true
+      disk_autoresize_limit   = 0
+      disk_size               = "10GB"
+      disk_type               = "PD_SSD"
+      pricing_plan            = "PER_USE"
+      user_labels             = {}
+      database_flags          = []
+      active_directory_config = []
+      deny_maintenance_period = []
+      backup_configuration    = []
+      ip_configuration        = []
+      location_preference     = []
+      maintenance_window      = []
+      insights_config         = []
+    }
+  ]
+}
+
+variable "database_version" {
+  type        = string
+  description = "The MySQL, PostgreSQL or SQL Server version to use. Supported values include MYSQL_5_6, MYSQL_5_7, MYSQL_8_0, POSTGRES_9_6,POSTGRES_10, POSTGRES_11, POSTGRES_12, POSTGRES_13, POSTGRES_14, SQLSERVER_2017_STANDARD, SQLSERVER_2017_ENTERPRISE, SQLSERVER_2017_EXPRESS, SQLSERVER_2017_WEB. SQLSERVER_2019_STANDARD, SQLSERVER_2019_ENTERPRISE, SQLSERVER_2019_EXPRESS, SQLSERVER_2019_WEB"
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the instance. If the name is left blank, Terraform will randomly generate one when the instance is first created. This is done because after a name is used, it cannot be reused for up to one week."
+}
+
+variable "maintenance_version" {
+  type        = string
+  description = "The current software version on the instance. This attribute can not be set during creation. Refer to available_maintenance_versions attribute to see what maintenance_version are available for upgrade. When this attribute gets updated, it will cause an instance restart. Setting a maintenance_version value that is older than the current one on the instance will be ignored."
+  default     = null
+}
+
+variable "master_instance_name" {
+  type        = string
+  description = "The name of the existing instance that will act as the master in the replication setup. Note, this requires the master to have binary_log_enabled set, as well as existing backups."
+  default     = null
+}
+
+variable "project" {
+  type        = string
+  description = "The ID of the project in which the resource belongs. If it is not provided, the provider project is used."
+}
+
+variable "replica_configuration" {
+  type = set(object({
+    ca_certificate            = string
+    client_certificate        = string
+    client_key                = string
+    connect_retry_interval    = number
+    dump_file_path            = string
+    failover_target           = bool
+    master_heartbeat_period   = number
+    password                  = string
+    sslCipher                 = string
+    username                  = string
+    verify_server_certificate = bool
+  }))
+  description = "The configuration for replication. The configuration is detailed below. Valid only for MySQL instances."
+  default     = []
+}
+
+variable "root_password" {
+  type        = string
+  description = "Initial root password. Required for MS SQL Server."
+}
+
+variable "encryption_key_name" {
+  type        = string
+  description = "The full path to the encryption key used for the CMEK disk encryption. Setting up disk encryption currently requires manual steps outside of Terraform. The provided key must be in the same region as the SQL instance. In order to use this feature, a special kind of service account must be created and granted permission on this key. This step can currently only be done manually, please see this step. That service account needs the Cloud KMS > Cloud KMS CryptoKey Encrypter/Decrypter role on your key - please see this step."
+  default     = null
+}
+
+variable "deletion_protection" {
+  type        = string
+  description = "Whether or not to allow Terraform to destroy the instance. Unless this field is set to false in Terraform state, a terraform destroy or terraform apply command that deletes the instance will fail. Defaults to true."
+  default     = true
+}
+
+variable "restore_backup_context" {
+  type = set(object({
+    backup_run_id = string
+    instance_id   = string
+    project       = string
+  }))
+  description = "The context needed to restore the database to a backup run. This field will cause Terraform to trigger the database to restore from the backup run indicated. The configuration is detailed below. NOTE: Restoring from a backup is an imperative action and not recommended via Terraform. Adding or modifying this block during resource creation/update will trigger the restore action after the resource is created/updated."
+  default     = []
+}
+
+variable "clone" {
+  type = set(object({
+    source_instance_name = string
+    point_in_time        = string
+    allocated_ip_range   = string
+  }))
+  description = "The context needed to create this instance as a clone of another instance. When this field is set during resource creation, Terraform will attempt to clone another instance as indicated in the context. The configuration is detailed below."
+  default     = []
+}
